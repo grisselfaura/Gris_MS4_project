53fde554601377c61fa667c95464dd6d28692857
grisselfaura <grissel.faura@gmail.com>
2020-11-23 19:07:38 +0000

    added blog base.html template, bootstraap, js  and using inheritance

--- MS4_Project/settings.py
+++ MS4_Project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
