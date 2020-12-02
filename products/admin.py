2b46af3392d15277bd7a4db0de1ba72c9ce9b1f7
grisselfaura <grissel.faura@gmail.com>
2020-12-02 16:30:22 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,32 @@ from django.contrib import admin
 from .models import Service, Category
 
 # Register your models here.
-admin.site.register(Service)
-admin.site.register(Category)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'research',
+        'design_sprint',
+        'usability_testing',
+        'price',
+        'is_a_service',
+        'duration',
+        'revision',
+        'image',
+        'discontinued',
+    )
+
+    ordering = ('price',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Category, CategoryAdmin)
