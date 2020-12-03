8b87c7a543ed84ab32822e957b4148b0d095fd6d
grisselfaura <grissel.faura@gmail.com>
2020-12-03 01:32:27 +0000

    added service_detail template, url, views

--- products/views.py
+++ products/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 # Create your views here.
 
 
 def all_services(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all services, including sorting and search queries """
 
     all_services = Service.objects.all()
 
     context = {
-        'services': all_services
+        'services': all_services,
     }
     return render(request, 'products/services.html', context)
 
+
+def service_detail(request, service_id):
+    """ A view to show individual service details """
+
+    service_detail = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service_detail,
+    }
+
+    return render(request, 'products/service_detail.html', context)
