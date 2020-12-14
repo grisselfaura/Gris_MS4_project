4d25c4fd50fdb6a39323130e496f3dbe12adf49d
grisselfaura <grissel.faura@gmail.com>
2020-12-14 22:27:29 +0000

    problem identified with profile, pending to fix

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
@@ -7,7 +9,7 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
-    delivery information and order history
+    billing information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -22,3 +24,12 @@ class UserProfile(models.Model):
         return self.user.username
 
 
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     """
+#     Create or update the user profile
+#     """
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#     # Existing users: just save the profile
+#     instance.userprofile.save()
