c8f2f10c1ecc2787d786eec0bf99dc648a3fb972
grisselfaura <grissel.faura@gmail.com>
2020-12-17 17:48:17 +0000

    Product admin - securing the views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -76,8 +77,13 @@ def service_detail(request, service_id):
     return render(request, 'products/service_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -97,8 +103,13 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, service_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     service = get_object_or_404(Service, pk=service_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=service)
@@ -121,8 +132,13 @@ def edit_product(request, service_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, service_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
     service = get_object_or_404(Service, pk=service_id)
     service.delete()
     messages.success(request, 'Service deleted!')
