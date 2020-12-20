967ec9fbcf6806b0a03f322fb90a5718de948de6
grisselfaura <grissel.faura@gmail.com>
2020-12-20 02:28:50 +0000

    portafolio html

--- portafolio/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def portafolio(request):
-    """ A view that renders the about me page """
-    return render(request, 'portafolio/portafolio.html', {'title': 'Portafolio'})
