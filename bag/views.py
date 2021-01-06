95c381c923e93b6c68f1794468cfc35e9bd75f67
grisselfaura <grissel.faura@gmail.com>
2021-01-06 03:00:19 +0000

    reformat add to bag and started update bag

--- bag/views.py
+++ bag/views.py
@@ -24,26 +24,29 @@ def add_to_bag(request, item_id):
     select_date = None
     if 'select_date' in request.POST:
         select_date = request.POST['select_date']
-
-    # Most clients customers would order one service at the time,
+    # Most clients customers would order one service at the time
     if select_date:
         if item_id in list(bag.keys()):
             if select_date in bag[item_id]['items_by_date'].keys():
-                messages.error(request, f' {all_services.name} scheduled for {select_date} already in your cart! \
-                    Please book a different timeslot for your next order.')
+                messages.error(request, f'{all_services.name} scheduled for {select_date} already in your bag! \
+                        Please book a different timeslot for your next order')
                 return redirect(redirect_url)
             else:
-                bag[item_id]['items_by_date'][select_date] = quantity
-                messages.success(request,
-                                 f'Added {all_services.name} to your bag')
-        else:
+                messages.error(request, f'{all_services.name} already in your bag! \
+                    Please give me a call to discuss the posibilities')
+                return redirect(redirect_url)
+        else: 
             bag[item_id] = {'items_by_date': {select_date: quantity}}
             messages.success(request, f'Added {all_services.name} to your bag')
-    else:
-        messages.error(request, f'Please select a date to discuss your project! \
-            Check your cart to continue booking.')
-        return redirect(redirect_url)
-
+    # else:
+    #     if item_id in list(bag.keys()):
+    #         messages.error(request, f'{all_services.name} already in your bag! \
+    #             Please give me a call to discuss the posibilities')
+    #         return redirect(redirect_url)
+    #     else:
+    #         bag[item_id]['items_by_date'][select_date] = quantity
+    #             messages.success(request,
+    #                              f'Added {all_services.name} to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -54,14 +57,17 @@ def adjust_bag(request, item_id):
     all_services = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
-
-    if quantity > 0:
-        bag[item_id] = quantity
-        messages.success(
-            request, f'Updated {all_services.name} quantity to {bag[item_id]}')
-    else:
-        bag.pop(item_id)
-        messages.success(request, f'Removed {all_services.name} from your bag')
+    select_date = None
+    if 'select_date' in request.POST:
+        select_date = request.POST['select_date']
+    if select_date:
+        if quantity > 0:
+            bag[item_id] = {'items_by_date': {select_date: quantity}}
+            messages.success(
+                request, f'Updated Successfully {all_services.name} quantity to {bag[item_id]}')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {all_services.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -82,3 +88,26 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+
+
+# Most clients customers would order one service at the time
+    # if select_date:
+    #     if item_id in list(bag.keys()):
+    #         if select_date in bag[item_id]['items_by_date'].keys():
+    #             messages.error(request, f'{all_services.name} scheduled for {select_date} already in your cart! \
+    #                 Please book a different timeslot for your next order')
+    #             return redirect(redirect_url)
+    #         else:
+    #             bag[item_id]['items_by_date'][select_date] = quantity
+    #             messages.success(request,
+    #                              f'Added {all_services.name} to your bag')
+    #     else:
+    #         bag[item_id] = {'items_by_date': {select_date: quantity}}
+    #         messages.success(request, f'Added {all_services.name} to your bag')
+    # else:
+    #     messages.error(request, f'Please select a date for your {all_services.name} to discuss your project! \
+    #         Check your cart to continue booking')
+    #     return redirect(redirect_url)
+
+    # request.session['bag'] = bag
+    # return redirect(redirect_url)
