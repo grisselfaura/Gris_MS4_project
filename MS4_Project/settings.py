3aa919cab1cb395b541c1037303966528dfdfc9f
grisselfaura <grissel.faura@gmail.com>
2021-01-15 23:31:46 +0000

    deployment part 2

--- MS4_Project/settings.py
+++ MS4_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'b2+9x=3cs*o9hu!rjp-2#1j@!e*gdzjm9d&0j)5c9^^rxg$54q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gris-ms4-project.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'MS4_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MS4_Project.wsgi:application
