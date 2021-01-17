97723fb805ec18d38fe4fb1abd1f68d9dec47c04
grisselfaura <grissel.faura@gmail.com>
2021-01-17 23:42:14 +0000

    portfolio imgs

--- products/forms.py
+++ products/forms.py
@@ -21,11 +21,3 @@ class ProductForm(forms.ModelForm):
             field.widget.attrs['class'] = 'border-black rounded-0'
 
 
-# class DateForm(forms.ModelForm):
-#     select_date = forms.DateTimeField(
-#         input_formats=['%d/%m/%Y %H:%M'],
-#         widget=forms.DateTimeInput(attrs={
-#             'class': 'form-control datetimepicker-input',
-#             'data-target': '#datetimepicker-demo'
-#         })
-#     )
