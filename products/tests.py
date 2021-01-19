ddfb8ae044b7a88a5788a69b1de860501b081859
grisselfaura <grissel.faura@gmail.com>
2021-01-19 01:36:18 +0000

    update readme and test for products

--- products/tests.py
+++ products/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
+from .models import Service
 
 # Create your tests here ADAPT TO OUR SERVICE
-# from .models import Product
 
 
-# class TestModel(TestCase):
+class TestModel(TestCase):
 
-#     def test_done_default_to_false(self):
-#         product = Product.objects.create(name="Test product", description="Test description",
-#                                          price=2.10)
-#         self.assertFalse(product.has_weight)
+    def test_done_default_to_false(self):
+        product = Service.objects.create(name="Test Service", description="Test Service description",
+                                         price=499)
+        self.assertFalse(product.is_a_service)
