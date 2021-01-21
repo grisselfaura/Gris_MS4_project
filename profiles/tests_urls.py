ede9a410fbad760e0c700d3225bbc119ada23f11
grisselfaura <grissel.faura@gmail.com>
2021-01-21 21:02:02 +0000

    added profile test.urls

---
+++ profiles/tests_urls.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from profiles.views import profile, order_history
+
+class TestUrls(SimpleTestCase):
+
+    def test_profile_url_resolves(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func, profile)
+
+    def test_order_history_url_resolves(self):
+        url = reverse('order_history', args=['some-number'])
+        self.assertEquals(resolve(url).func, order_history)
+
