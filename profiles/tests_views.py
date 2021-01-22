cffa1b851c343a3e7a1c303745cc6f6db14939f4
grisselfaura <grissel.faura@gmail.com>
2021-01-22 00:56:19 +0000

    testing profile view

--- profiles/tests_views.py
+++ profiles/tests_views.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from profiles.models import UserProfile, create_or_update_user_profile
+import json
+# from django.contrib.auth.models import User, AnonymousUser
+# from django.shortcuts import get_object_or_404
+# from accounts.views import register, login, logout, profile
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_profile_page_GET(self):
+        """
+        Tests that the login page view renders the login template
+        """
+        response = self.client.get(reverse('profile'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
