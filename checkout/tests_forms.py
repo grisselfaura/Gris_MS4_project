d280ad5e8a02b36d35a8adccaf992934fa1ccf3d
grisselfaura <grissel.faura@gmail.com>
2021-01-22 04:29:10 +0000

    started testing in test form checkout

--- checkout/tests_forms.py
+++ checkout/tests_forms.py
@@ -2,20 +2,20 @@ from django.test import TestCase
 from .forms import OrderForm
 
 
-class TestOrderForm(TestCase):
+class TestCheckoutOrderForm(TestCase):
 
-    def test_create_order_with_required_fields_filled(self):
+    def test_generate_order_with_required_fields(self):
         form = OrderForm({
-            'full_name': 'test',
-            'email': 'test@gmail.com',
-            'phone_number': '123456 789',
+            'full_name': 'Carlos Ramos',
+            'email': 'cramos@gmail.com',
+            'phone_number': '+31 123456 789',
             'address_line1': '123 test street',
             'town_or_city': 'test city',
             'country': 'NL'})
         self.assertTrue(form.is_valid())
 
-    def test_correct_message_for_empty_form(self):
+    def test_generate_correct_message_for_empty_form(self):
         form = OrderForm({'form': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['full_name'],
-                         [u'This field is required.'])
+                            [u'This field is required.'])
