4090a6e2c827aeed51db4830dfddc22b3107b02d
grisselfaura <grissel.faura@gmail.com>
2021-01-26 02:49:07 +0000

    Added missing views links for 404/500 pages

--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def view_404(request, exception):
+    return render(request, 'templates/404.html')
+
+
+def view_500(request):
+    return render(request, 'templates/500.html')
