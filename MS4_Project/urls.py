9ed4d1582329f302d5db36c548dabcd3b5352e58
grisselfaura <grissel.faura@gmail.com>
2021-01-26 03:57:26 +0000

    testing 404 resize

--- MS4_Project/urls.py
+++ MS4_Project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +30,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'home.views.view_404'
+handler500 = 'home.views.view_500'
